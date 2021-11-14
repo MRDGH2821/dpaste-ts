dc290c4a5a5dcd58c0a7e028cf90d19201cb0456
Mihir Rabade <mihirrabade@gmail.com>
2021-11-14 15:48:57 +0530

    Added new function & JS Doc

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 const axios = require('axios').default;
-import { Lexer, Expires, jsdata } from '../src/lib/Interfaces';
+import { Lexer, Expires, PasteFormat } from '../lib/Interfaces';
+
+/**
+* Creates Paste on dpaste.org
+* @async
+* @function CreatePaste
+* @param {string} content - The paste data
+* @param {string} filename - The title for Paste
+* @param {Lexer} lexer - Paste encoding
+* @param {Expires} expires - Expiry duration of the paste
+* @returns {PasteFormat}
+*/
 async function CreatePaste(content: string, filename: string, lexer?: Lexer, expires?: Expires) {
 	await axios({
 		url: "https:/dpaste.de/api/",
-    method: "post",
+		method: "post",
 		params: {
 			content: content,
 			filename: filename,
@@ -12,10 +23,22 @@ async function CreatePaste(content: string, filename: string, lexer?: Lexer, exp
 			format: JSON,
 		}
 	})
-		.then((jsdata: jsdata) => { return jsdata; })
-		.catch((error: string) => { return error; })
+		.then((jsdata: PasteFormat) => { return jsdata; })
+		.catch((error: PasteFormat) => { return error; })
+}
+
+/**
+* Creates Paste on dpaste.org
+* @async
+* @function GetPaste
+* @param {string} url - The dpaste url 
+*/
+async function GetPaste(url: string) {
+	const data = await axios.get(url);
+	console.log(data);
 }
 
 module.exports = {
-  CreatePaste,
+	CreatePaste,
+	GetPaste
 }
