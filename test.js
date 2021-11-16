e98287fe754d60a64706cd7e8b6df67262af1c4c
Mihir Rabade <mihirrabade@gmail.com>
2021-11-16 16:54:27 +0530

    updated test code

--- test.js
+++ test.js
@@ -1,15 +1,9 @@
 const {CreatePaste, GetPaste} = require('dpaste-js');
 const fetch = require('cross-fetch').default;
+const axios = require('axios').default;
 
-const data = 'Some sample data for testing dpaste module in plain text using axios';
+const data = 'Some sample data for testing dpaste module in plain text using axios. Now with proper error handling';
 const title = 'Dpaste Module Axios Test';
 CreatePaste(data, title, 'text').then(console.log).catch(console.log);
 
-//GetPaste('https://dpaste.com/F5VXWRTCA.txt').then(console.log);
-/*
-const dat = fetch('https://dpaste.com/45WYTLHUK.txt')
-dat.then(val => {
-  console.log(val.headers)
-})
-
-*/
+GetPaste('https://dpaste.com/F5VXWRTCA').then(console.log);
