00a7cc32e3013fbdab7f51d85b6d537f6675c493
Mihir Rabade <mihirrabade@gmail.com>
2021-11-16 10:38:07 +0530

    Updated test code

--- test.js
+++ test.js
@@ -1,5 +1,12 @@
-const {CreatePaste} = require('dpaste-js');
-
-const data = 'Some  sample data for testing dpaste module';
+const {CreatePaste, GetPaste} = require('dpaste-js');
+const fetch = require('cross-fetch').default;
+/*
+const data = 'Some sample data for testing dpaste module in js';
 const title = 'Dpaste Module Test';
-CreatePaste(data, title).then(console.log);
+CreatePaste(data, title, 'js', 3).then(console.log);
+*/
+//GetPaste('https://dpaste.com/F5VXWRTCA.txt').then(console.log);
+const dat = fetch('https://dpaste.com/45WYTLHUK.txt')
+dat.then(val => {
+  console.log(val.headers)
+})
