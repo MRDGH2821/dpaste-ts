64db13cdd0097ff635cfc1cf918c6731422f6b43
MRDGH2821 <ask.mrdgh2821@outlook.com>
2021-11-16 10:37:25 +0530

    feat: add expiry date option
      Rename parameters

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 const axios = require('axios').default;
 //const qs = require('qs');
-import { Lexer, Expires, PasteFormat } from '../lib/Interfaces';
+const fetch = require('cross-fetch').default;
+import { Syntax, Expiry_Days } from '../lib/Interfaces';
 //const FormData = require('form-data');
 
 //const form = new FormData();
@@ -10,50 +11,17 @@ import { Lexer, Expires, PasteFormat } from '../lib/Interfaces';
 * @function CreatePaste
 * @param {string} content - The paste data
 * @param {string} filename - The title for Paste
-* @param {Lexer} lexer - Paste encoding
-* @param {Expires} expires - Expiry duration of the paste
-* @returns {Promise<PasteFormat>}
+* @param {Syntax} syntax - Paste encoding
+* @param {Expiry_Days} expiry_days - Expiry duration of the paste
+* @returns {Promise<String>}
 */
-export async function CreatePaste(content: string, filename: string, lexer: Lexer = 'text', expires: Expires = '2592000'): Promise<String> {
+export async function CreatePaste(content: string, filename: string, syntax: Syntax = 'text', expiry_days: Expiry_Days = 7): Promise<String> {
 	const res = await fetch('https://dpaste.com/api/v2/', {
-
 		method: 'POST',
 		headers: { "Content-Type": "application/x-www-form-urlencoded" },
-		body: "content=" + encodeURIComponent(content) + "&syntax=" + encodeURIComponent(lexer) + "&title=" + encodeURIComponent(filename),
+		body: "content=" + encodeURIComponent(content) + "&syntax=" + encodeURIComponent(syntax) + "&title=" + encodeURIComponent(filename) + "&expiry_days" + encodeURIComponent(expiry_days),
 	})
 	return res.text();
-	/*
-		.then((jsdata: PasteFormat) => { return jsdata; })
-		.catch((error: PasteFormat) => { return error; })
-	*/
-	/*
-	const form = new FormData();
-	form.append('content', content);
-	form.append('lexer', lexer);
-	form.append('format', 'JSON');
-	form.append('expires', expires);
-	form.append('filename', filename);
-	return await axios({
-		url: "https://dpaste.com/api/",
-		method: 'post',
-		headers: {
-			'Content-Type': "multipart/form-data",
-		}
-	})
-		.then((jsdata: PasteFormat) => { return jsdata; })
-		.catch((error: PasteFormat) => { return error; })
-
-		method: "post",
-		data: {
-			content: content,
-			lexer: lexer,
-			format: JSON,
-			expires: expires,
-			filename: filename
-		}
-
-	})
-	*/
 }
 
 /**
