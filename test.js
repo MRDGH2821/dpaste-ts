ab77061bde4a5cd07600df8e9a8a8a3d7cef6358
MRDGH2821 <ask.mrdgh2821@outlook.com>
2021-11-16 19:14:53 +0530

    test: add default link for tests

--- test.js
+++ test.js
@@ -3,11 +3,12 @@ const { CreatePaste, GetPaste } = require("dpaste-js");
 const data =
 	"Some sample data for testing dpaste module in plain text using axios. Now with proper error handling";
 const title = "Dpaste Module Axios Test";
-let url;
+let url = "https://dpaste.com/7TLQB5P7B";
+
 CreatePaste(data, title, "text", 1)
 	.then(data => {
 		url = data;
-    console.log(data);
+		console.log(data);
 	})
 	.catch(console.log);
 
