e4e5f406f85714de13927c40fa94b6415d13d3ff
Mihir Rabade <mihirrabade@gmail.com>
2021-11-18 18:59:28 +0530

    Added 260kb test code

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const { CreatePaste, GetRawPaste } = require("dpaste-ts");
-
+var fs = require("fs");
 const data =
 	"Some sample data for testing dpaste module in plain text using axios. Now with proper error handling";
 const title = "Dpaste Module Axios Test";
@@ -13,3 +13,15 @@ CreatePaste(data, title, "text", 1)
 	.catch(console.log);
 
 GetRawPaste(url).then(console.log);
+
+//let data260kbsize: number;
+fs.readFile("./test/260KB.txt", function(err, data) {
+	if (err) {
+		return console.error(err);
+	}
+	CreatePaste(data.toString(), (expiry_days = 1))
+		.then(console.log)
+		.catch(console.log);
+
+	//  data260kbsize = Buffer.byteLength(data260kb);
+});
