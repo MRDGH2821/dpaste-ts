a1b3934cb0f91141849396c381b000324b310bf0
MRDGH2821 <ask.mrdgh2821@outlook.com>
2022-10-31 23:33:09 +0530

    feat: add custom https request library

---
+++ src/lib.ts
@@ -0,0 +1,36 @@
+import { request, RequestOptions } from 'https';
+
+import { ResponseObject } from './interfaces';
+
+export default function httpsRequest(
+  urlOptions: RequestOptions,
+  inputData = '',
+): Promise<ResponseObject> {
+  return new Promise((resolve, reject) => {
+    const req = request(urlOptions, (res) => {
+      const chunks: unknown[] = [];
+
+      res.on('data', (chunk) => chunks.push(chunk));
+      res.on('error', reject);
+      res.on('end', () => {
+        const { statusCode, headers } = res;
+        const validResponse = statusCode && statusCode >= 200 && statusCode <= 299;
+        const body = chunks.join('').trim();
+
+        if (validResponse) {
+          resolve({ statusCode, headers, body });
+        } else {
+          reject(
+            new Error(
+              `Request failed. Status: ${statusCode} \n\nHeaders: ${headers} \n\nBody: ${body}`,
+            ),
+          );
+        }
+      });
+    });
+
+    req.on('error', reject);
+    req.write(inputData);
+    req.end();
+  });
+}
