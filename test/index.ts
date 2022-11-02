957d34097ef1ecb1e6df9d700963c78e6dc695dc
MRDGH2821 <ask.mrdgh2821@outlook.com>
2022-11-02 18:37:32 +0530

    chore: update test cases

--- test/index.ts
+++ test/index.ts
@@ -12,17 +12,17 @@ API.run();
 const data = 'Some sample data for testing dpaste module using uvu';
 const title = 'Dpaste Module Test';
 
-const url = paste.CreatePaste(data, title);
+const url = paste.createPaste({ content: data, title });
 
 const create = suite('create');
 
 create('Should create a paste & return url', async () => {
-  assert.type(await paste.CreatePaste(data, title), 'string');
+  assert.type(await paste.createPaste({ content: data, title }), 'string');
 });
 create('Should throw error on invalid syntax', async () => {
   try {
     // @ts-ignore
-    await paste.CreatePaste(data, title, 'plain', 1);
+    await paste.createPaste(data, title, 'plain', 1);
     assert.unreachable('Should have thrown error');
   } catch (error) {
     assert.instance(error, Error);
@@ -34,12 +34,12 @@ create.run();
 const get = suite('get');
 
 get('Should get a paste & return String', async () => {
-  assert.type(await paste.GetRawPaste(await url), 'string');
+  assert.type(await paste.getRawPaste(await url), 'string');
 });
 
 get('Should throw error on invalid input', async () => {
   try {
-    await paste.GetRawPaste('sgfsgsg');
+    await paste.getRawPaste('sgfsgsg');
     assert.unreachable('Should have thrown error');
   } catch (error) {
     assert.instance(error, Error);
--- test/selfTest.ts
+++ test/selfTest.ts
@@ -1,6 +1,6 @@
 import * as dp from '../src/index';
 
-dp.CreatePaste('sample').then((url) => {
+dp.createPaste({ content: 'sample' }).then((url) => {
   console.log(url);
-  dp.GetRawPaste(url).then(console.log);
+  dp.getRawPaste(url).then(console.log);
 });
