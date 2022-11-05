d9bd1ba6753e94a6473ffe5569b7c7b9d1f2ee82
MRDGH2821 <ask.mrdgh2821@outlook.com>
2022-11-05 15:01:10 +0530

    chore: add blocking delay

--- test/index.ts
+++ test/index.ts
@@ -2,6 +2,15 @@ import { suite } from 'uvu';
 import * as assert from 'uvu/assert';
 import * as paste from '../src';
 
+async function delay() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('1 second wait done.');
+      resolve(1);
+    }, 1000);
+  });
+}
+
 const API = suite('exports');
 
 API('should export an object', () => {
@@ -17,15 +26,11 @@ const url = paste.createPaste({ content: data, title });
 const create = suite('create');
 
 create('Should create a paste & return url', async () => {
-  setTimeout(() => {
-    console.log('1 second wait done.');
-  }, 1000);
+  await delay();
   assert.type(await paste.createPaste({ content: data, title }), 'string');
 });
 create('Should throw error on invalid syntax', async () => {
-  setTimeout(() => {
-    console.log('1 second wait done.');
-  }, 1000);
+  await delay();
   try {
     await paste.createPaste({
       content: data,
@@ -45,16 +50,12 @@ create.run();
 const get = suite('get');
 
 get('Should get a paste & return String', async () => {
-  setTimeout(() => {
-    console.log('1 second wait done.');
-  }, 1000);
+  await delay();
   assert.type(await paste.getRawPaste(await url), 'string');
 });
 
 get('Should throw error on invalid input', async () => {
-  setTimeout(() => {
-    console.log('1 second wait done.');
-  }, 1000);
+  await delay();
   try {
     await paste.getRawPaste('sgfsgsg');
     assert.unreachable('Should have thrown error');
@@ -67,9 +68,7 @@ get.run();
 const createAuth = suite('createAuth');
 
 createAuth('Should be able to create paste with API Token', async () => {
-  setTimeout(() => {
-    console.log('1 second wait done.');
-  }, 1000);
+  await delay();
   const urlAuth = await paste.createPaste({
     content: `dpaste-ts test case, performed on ${new Date().toUTCString()}`,
     APIToken: process.env.DPASTE_API_TOKEN_GIT,
